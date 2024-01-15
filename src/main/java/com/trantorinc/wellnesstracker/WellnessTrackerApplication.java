4f7265cf6bb77c8f592172c0aa337fb8514086d7
Vibhor Mahajan <vmahajan@lendingpoint.com>
2024-01-15 16:58:49 +0530

    Init

---
+++ src/main/java/com/trantorinc/wellnesstracker/WellnessTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.trantorinc.wellnesstracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WellnessTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WellnessTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trantorinc/wellnesstracker/WellnessTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trantorinc.wellnesstracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WellnessTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
